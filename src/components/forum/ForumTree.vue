76c5682df59e1eb3251eeb18fd1af2b3c363d2f1
Zhang Yao <cineazhan@gmail.com>
2023-10-26 00:35:59 +0800

    暂存工作

--- src/components/forum/ForumTree.vue
+++ src/components/forum/ForumTree.vue
@@ -69,8 +69,8 @@ const loading = ref(false)
 
 const route = useRoute()
 
-const expId = ref([(<String>route.params["id"]).replace(/-/g, "/")])
-const selId = ref([(<String>route.params["id"]).replace(/-/g, "/")])
+const expId = ref([(<String>route.params["id"])?.replace(/-/g, "/")] ?? "")
+const selId = ref([(<String>route.params["id"])?.replace(/-/g, "/")] ?? "")
 
 onMounted(() => {
   loading.value = true
